57b12df17f34f24cf56abaadf587f9ad4c11d292
Konstantinos Giannoulas <kgian02@gmail.com>
2023-11-15 22:29:43 +0200

    c6 - Added average rainfall function

--- rainfall.f95
+++ rainfall.f95
@@ -3,17 +3,19 @@ implicit none
 
 character(30)::Town(50)
 integer::Rain(50)
-integer::st,i
+integer::st,i,Mesos
 
 
 i=1
+Mesos=0
 open(10,file='data.txt',status='old',iostat=st)
 read(10,*) !prospernao thn proth grammh toy arxeiou
 do 
 
   read (10,'(A20,2x,i3)',iostat=st) Town(i),Rain(i)
     if (st==-1) exit 
-  write(*,'(A20,2x,i3)') Town(i),Rain(i)   
+  write(*,'(A20,2x,i3)') Town(i),Rain(i) 
+  Mesos = Mesos + Rain(i)  
   i=i+1
 
   
@@ -23,5 +25,5 @@ end do
 close (10)
 
 write (*,'(A,2x,i2,2x,A)') 'Exoume',i,'Poleis kai ta dedomena tous gia broxh'
-
+write (*,'(A,2x,i5)') 'O mesos oros broxhs stis poleis einai',Mesos
 end program
