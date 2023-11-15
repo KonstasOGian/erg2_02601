dc41c9135c089759c443fa15519259990b93fe2f
Konstantinos Giannoulas <kgian02@gmail.com>
2023-11-15 18:28:34 +0200

    c4 - Read and display all items (up to 50) from data.txt

--- rainfall.f95
+++ rainfall.f95
@@ -1,27 +1,27 @@
 program Rainfall
 implicit none
 
-character(30)::Town(5)
-integer::Rain(5)
+character(30)::Town(50)
+integer::Rain(50)
 integer::st,i
 
-i=0
+
+i=1
 open(10,file='data.txt',status='old',iostat=st)
 read(10,*) !prospernao thn proth grammh toy arxeiou
 do 
-  i=i+1
-  read (10,'(A20,2x,i3)',iostat=st) Town(i),Rain(i)
-  
-  write(*,'(A20,2x,i3)') Town(i),Rain(i)   
 
- if(i==5) then
-  exit
- end if  
+  read (10,'(A20,2x,i3)',iostat=st) Town(i),Rain(i)
+    if (st==-1) exit 
+  write(*,'(A20,2x,i3)') Town(i),Rain(i)   
+  i=i+1
+
+  
 !write(*,'(A20)') Town(i)
 !write(*,'(i3)') Rain(i)
 end do    
 close (10)
 
-
+write (*,'(A,2x,i2,2x,A)') 'Exoume',i,'Poleis kai ta dedomena tous gia broxh'
 
 end program
